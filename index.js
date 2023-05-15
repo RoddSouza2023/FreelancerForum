e4959147c4dffd41a7d3fcec02b74d442e0d4f06
Rodrigo Souza <roddsouza@hotmail.com>
2023-05-15 17:16:54 -0400

    upload block20 files

--- index.js
+++ index.js
@@ -7,11 +7,36 @@ const users = [
     { name: "Albert", age: 76, occupation: "programmer" },
     { name: "Adam", age: 47, occupation: "teacher" },
     { name: "Robert", age: 72, occupation: "driver" },
-];
+]
 
 function main() {
+    let root = document.getElementById("root")
+    let h1 = document.createElement("h1")
+    h1.setAttribute("id", "pageTitle")
+    h1.innerHTML = "FREELANCERS"
+    root.appendChild(h1)
 
+    let ul = document.createElement("ul")
+    ul.setAttribute("id", "theList")
+
+    for (const person of users) {
+        let li = document.createElement("li")
+        li.innerHTML = `${person.name} is ${person.age} 
+            years old and works as a ${person.occupation}`
+        ul.appendChild(li)
+    }
+
+    root.appendChild(ul)
+}
+
+function hovering() {
+const lis = document.querySelectorAll('li')
+lis.forEach((li) => {
+  li.style.setProperty('--td-background-color', '#684BE3');
+});
 }
 
 //call the main function
-main();
+main()
+hovering()
+
